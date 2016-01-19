c7d37e76f05fc1be6e9e896ecb07ce7507fb60c6
JuuPee <juhapekka.tiirikainen@gmail.com>
2016-01-19 18:41:16 +0200

    Initial kommit.

---
+++ Tehtävä_3/Tehtävä_3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtävä_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int luku1, luku2, luku3;
+            int summa, keskiarvo;
+
+            Console.Write("Anna luku > ");
+            luku1 = int.Parse(Console.ReadLine());
+            Console.Write("Anna luku > ");
+            luku2 = int.Parse(Console.ReadLine());
+            Console.Write("Anna luku > ");
+            luku3 = int.Parse(Console.ReadLine());
+
+            summa = luku1 + luku2 + luku3;
+            keskiarvo = summa / 3;
+
+            Console.WriteLine("Lukujen summa on " + summa);
+            Console.WriteLine("Ja keskiarvo " + keskiarvo);
+
+
+        }
+    }
+}
---
+++ Tehtävä_4/Tehtävä_4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtävä_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ika;
+
+            Console.WriteLine("Anna ikäsi: ");
+            ika = int.Parse(Console.ReadLine());
+
+            if (ika < 18)
+            {
+                Console.WriteLine("Alaikäinen ");
+            }
+            else if (ika < 65)
+            {
+                Console.WriteLine("Aikuinen  ");
+            } 
+            else
+            {
+                Console.WriteLine("Seniori ");
+            }
+        }
+    }
+}
