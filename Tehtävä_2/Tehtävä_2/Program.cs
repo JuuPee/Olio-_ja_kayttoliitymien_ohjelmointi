988dc081615c07775699861b8ea811929285b4ee
JuuPee <juhapekka.tiirikainen@gmail.com>
2016-01-19 18:25:02 +0200

    Initial kommit.

---
+++ Tehtävä_2/Tehtävä_2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtävä_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int points;
+
+            Console.Write("Anna luku > ");
+            points = int.Parse(Console.ReadLine());
+           
+            switch (points)
+            {
+                case 0:
+                case 1:
+                    Console.Write("Sait arvosanan 0.");
+                    break;
+                case 2:
+                case 3:
+                    Console.Write("Sait arvosanan 1.");
+                    break;
+                case 4:
+                case 5:
+                    Console.Write("Sait arvosanan 2.");
+                    break;
+                case 6:
+                case 7:
+                    Console.Write("Sait arvosanan 3.");
+                    break;
+                case 8:
+                case 9:
+                    Console.Write("Sait arvosanan 4.");
+                    break;
+                case 10:
+                case 11:
+                case 12:
+                    Console.Write("Sait arvosanan 5.");
+                    break;
+                
+            }
+                
+            }
+                
+        }
+    }
+
