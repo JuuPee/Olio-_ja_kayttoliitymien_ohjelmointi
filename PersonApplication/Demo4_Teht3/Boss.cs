0a0fb35dad6dc7b0b41414a518967146fa881eed
JuuPee <juhapekka.tiirikainen@gmail.com>
2016-01-26 19:45:21 +0200

    Initial kommit.

---
+++ PersonApplication/Demo4_Teht3/Boss.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo4_Teht3
+{
+    class Boss : Employee
+    {
+        public string Car { get; set; }
+        public float Bonus { get; set; }
+
+        public Boss(string name, string profession, float salary, string car, float bonus)
+            : base (name, profession, salary)
+            {
+            Car = car;
+            Bonus = bonus;
+    }
+        public override void Work()
+        {
+            Console.WriteLine("Working. But not so much anymore. ");
+        }
+    }
+}
---
+++ PersonApplication/Demo4_Teht3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo4_Teht3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee kirsi = new Employee("Kirsi", "Teacher", 4200.50f);
+            Boss jussi = new Boss("Jussi", "Boss", 5000.0f, "Audi", 5000.0f);
+
+            kirsi.Salary += 1000.0f;
+            jussi.Salary -= 2000.0f;
+            jussi.Bonus = 1000.0f;
+            // kirsi.Bonus = nocando
+
+            ShowSomeWork(kirsi);
+            ShowSomeWork(jussi);
+
+            Console.WriteLine();
+        }
+        static void ShowSomeWork(Employee empl)
+        {
+            empl.Work();
+        }
+    }
+}
